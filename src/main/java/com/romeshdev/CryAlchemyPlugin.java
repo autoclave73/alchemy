c95e43db0f9e39568d3bb1b72476d783c33fd320
autoclave73 <targeted.individual73@gmail.com>
2025-12-24 14:14:33 +0100

    Update CryAlchemyPlugin.java

--- src/main/java/com/romeshdev/CryAlchemyPlugin.java
+++ src/main/java/com/romeshdev/CryAlchemyPlugin.java
@@ -1,4 +1,4 @@
-package com.romeshdev;
+package org.ye;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.events.GraphicChanged;
 import net.runelite.client.eventbus.Subscribe;
@@ -6,27 +6,26 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @PluginDescriptor(
-        name = "Cry Alchemy"
+		name = "Cry Alchemy"
 )
 @Slf4j
 public class CryAlchemyPlugin extends Plugin {
 
-    private static final int CRY_EMOTE_ANIMATION_ID = 860;
-    private static final int LOW_LEVEL_ALCHEMY_GFX_ID = 112;
-    private static final int HIGH_LEVEL_ALCHEMY_GFX_ID = 113;
-    private static final int ALCHEMY_GFX_HEIGHT_OFFSET = 100;
+	private static final int ALCHEMY_ANIMATION_ID = 1162;
+	private static final int HIGH_LEVEL_ALCHEMY_GFX_ID = 113;
+	private static final int ALCHEMY_GFX_HEIGHT_OFFSET = 100;
 
-    @Subscribe
-    public void onGraphicChanged(GraphicChanged graphicChange)
-    {
-        var actor = graphicChange.getActor();
-        var graphic = actor.getGraphic();
-        if(graphic == LOW_LEVEL_ALCHEMY_GFX_ID || graphic == HIGH_LEVEL_ALCHEMY_GFX_ID)
-        {
-            actor.setAnimation(CRY_EMOTE_ANIMATION_ID);
-            actor.setGraphic(LOW_LEVEL_ALCHEMY_GFX_ID);
-            actor.setGraphicHeight(ALCHEMY_GFX_HEIGHT_OFFSET);
-            actor.setSpotAnimFrame(0);
-        }
-    }
+	@Subscribe
+	public void onGraphicChanged(GraphicChanged graphicChange)
+	{
+		var actor = graphicChange.getActor();
+		var graphic = actor.getGraphic();
+		if(graphic == HIGH_LEVEL_ALCHEMY_GFX_ID)
+		{
+			actor.setAnimation(ALCHEMY_ANIMATION_ID);
+			actor.setGraphic(HIGH_LEVEL_ALCHEMY_GFX_ID);
+			actor.setGraphicHeight(ALCHEMY_GFX_HEIGHT_OFFSET);
+			actor.setSpotAnimFrame(0);
+		}
+	}
 }
